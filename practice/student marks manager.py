d1e96d9964175432ca93a26bd3e471ed1ae4f38e
Arnav95Dev <arnavmahajan956@gmail.com>
2026-06-26 19:44:43 +0530

    Basic excercise 2

---
+++ practice/student marks manager.py
@@ -0,0 +1,5 @@
+marks = [85, 90, 70, 95]
+marks.sort()
+print(f"Highest mark is:{marks[3]}")
+print(f"Lowest mark is:{marks[0]}")
+print(f"Average mark is:{sum(marks)/len(marks)}")
---
+++ practice/to do list.py
@@ -0,0 +1,7 @@
+a = str(input("Enter a task: "))
+b = str(input("Enter another task: "))
+c = str(input("Enter another task: "))
+d = str(input("Enter another task: "))
+
+task = [a,b,c,d]
+print(f"Your task are now stored in a list {task}")
